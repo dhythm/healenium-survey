7900375b5e77d4e5a271ba6d45ca3da1c0d50a65
dhythm <yuta.okada.20@gmail.com>
2024-02-03 15:38:13 +0900

    implement healenium-test

--- healenium-test/main.py
+++ healenium-test/main.py
@@ -1,1 +1,31 @@
-print("hello, world")
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def main():
+    driver = webdriver.Chrome()
+
+    node_url = "http://localhost:8085"
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+
+    current_driver = webdriver.Remote(
+        command_executor=node_url,
+        options=options,
+    )
+
+    driver.get("http://localhost:5173")
+    time.sleep(1)
+    element = driver.find_element(By.ID, "counter_button")
+    element.click()
+    time.sleep(1)
+    driver.quit()
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    elapsed_time = time.time() - start
+    print("elapsed_time: {0:0.2f}".format(elapsed_time) + "[sec]")
